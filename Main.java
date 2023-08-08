ae951570c789cf1d902f07ee36f00e29168312df
Aditya Kumar <CodifyWithAditya@Gmail.com>
2023-08-08 21:37:51 +0530

    learned about print, println, \n

---
+++ Main.java
@@ -0,0 +1,29 @@
+public class Main{
+    public static void main(String args[]){
+        
+        System.out.print("Hello World1");        
+        System.out.print("Hello World2");
+
+        System.out.println("Hello World 3");       
+        System.out.println("Hello World 4");
+
+        System.out.print("Hello World\n5");        
+        System.out.print("Hello World\n6");
+  
+        System.out.println("Hello World\n7\n Hello World8");        
+        System.out.println("Hello World\n9");
+ 
+// output
+// Hello World1Hello World2Hello World 3
+// Hello World 4
+// Hello World
+// 5Hello World
+// 6Hello World
+// 7
+//  Hello World8
+// Hello World
+// 9
+
+
+    }
+}
