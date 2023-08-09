19029c91ad7930115ba646089d39a29e3a391e61
Aditya Kumar <CodifyWithAditya@Gmail.com>
2023-08-09 21:23:32 +0530

    variables in java...adi9aug23

---
+++ Variables.java
@@ -0,0 +1,15 @@
+public class Variables {
+    public static void main(String[] args){
+        // variables
+        String name= "tony stark";
+        int age= 48;
+        int a= 25;
+        int b=10;
+        double price = 25.25;
+        
+        b=20;
+        name = "iron man";
+        
+
+    }
+}
