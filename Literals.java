e4e306b1dd7af705d030e14dae492be369ef7a68
Aditya Kumar <who.aditya@gmail.com>
2024-08-29 20:32:35 +0530

    varibale, literals, datatypes...adi 29aug24

---
+++ Literals.java
@@ -0,0 +1,32 @@
+public class Literals {
+    public static void main(String a[]){
+        
+        int num = 0b101; //decimal
+        System.out.println(num);
+        // 5 
+
+         
+        int num1 = 0x7E; //hexadecimal
+        System.out.println(num1);
+        // 126
+
+           
+        int num2 = 10_00_000; //amount
+        System.out.println(num2);
+        // 1000000
+
+        double d1 = 56;
+        System.out.println(d1);
+        // 56.0
+
+        double d2 = 12e10;
+        System.out.println(d2);
+        // 1.2E11
+
+        char c = 'a';
+        c++;
+        System.out.println(c);
+        // b
+
+    }
+}
---
+++ Variables.java
@@ -0,0 +1,10 @@
+class Variables {
+
+    public static void main(String a[]){
+        int num1 = 3;
+        int num2 = 6;
+        int result = num1 + num2;
+        System.out.println(result);
+    }
+    
+}
